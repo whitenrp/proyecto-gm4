3ab2a6f0486c39c4a7c618d3f17725eca1b57546
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2020-12-17 18:27:51 +0100

    Create monitor.cc

---
+++ monitor.cc
@@ -0,0 +1,11 @@
+#include "monitor.h"
+
+Monitor(string DNI, string nombre, string apellidos, int edad, string correoelectronico, string direccion)
+{
+  DNI_ = DNI;
+	nombre_ = nombre;
+	apellidos_ = apellidos;
+	edad_ = edad; 
+	direccion_ = direccion;
+	correoelectronico_=correoelectronico;
+}
