e3826fdb411fa9ad07b9198cd45cfd0584eecfbb
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2020-12-19 00:58:56 +0100

    Update monitor.cc

--- monitor.cc
+++ monitor.cc
@@ -2,10 +2,36 @@
 
 Monitor(string DNI, string nombre, string apellidos, int edad, string correoelectronico, string direccion)
 {
-  DNI_ = DNI;
+ 	DNI_ = DNI;
 	nombre_ = nombre;
 	apellidos_ = apellidos;
 	edad_ = edad; 
 	direccion_ = direccion;
 	correoelectronico_=correoelectronico;
 }
+
+bool Monitor::confirmar_monitor(){
+	string respuesta;
+	cout<<"¿Puedes asistir?";
+	getline(cin,respuesta);
+	if (respuesta=="si"){
+		return true;
+	}
+	else
+		return false;
+}
+
+void Monitor::setBuscar_rutas_disponibles(){
+}
+
+string Monitor::getDar_rutas(){
+}
+void Monitor::setBuscar_fechayhoradisp(){
+}
+string Monitor::getDar_fechayhoradisp(){
+}
+void Monitor::setBuscar_rutas_especiales(){
+}
+void Monitor::setReservar_fechayhora(){
+}
+
