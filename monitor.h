359d3976079fe3990abc09e5148f9f5ea0879559
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2020-12-21 12:26:04 +0100

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
+#include <stdlib.h>
 using namespace std;
 
 class Monitor{
@@ -14,30 +15,34 @@ string direccion_;
 
 
 public:
-    Monitor(string DNI, int edad=0, string nombre="", string apellidos="", string correoelectronico, string direccion)
-
+    Monitor(){
+        string DNI_="";
+        int edad_=0;
+        string nombre_="";
+        string apellidos_="";
+        string correoelectronico_="";
+        string direccion_="";
+    }
     inline void setEdad(int edad){edad=edad_;}
-    inline int getEdad(){return edad;}
+    inline int getEdad(){return edad_;}
     inline void setNombre(string nombre){nombre=nombre_;}
     inline string getNombre(){return nombre_;}
     inline void setApellidos(string apellidos){apellidos=apellidos_;}
     inline string getApellidos(){return apellidos_;}
-    inline void setDNI(string dni){DNI=DNI_;}
+    inline void setDNI(string DNI){DNI=DNI_;}
     inline string getDNI(){return DNI_;}
     inline void setCorreoelectronico(string correoelectronico){correoelectronico=correoelectronico_;}
     inline string getCorreoelectronico(){return correoelectronico_;}
     inline void setDireccion(string direccion){direccion=direccion_;}
     inline string getDireccion(){return direccion_;}
-  
-    inline void setDar_datos_monitor(string nombre, string apellidos, string DNI, int edad, string correoelectronico, string direccion){nombre=nombre_,apellidos=apellidos_,DNI=DNI_,edad=edad_,correoelectronico=correoelectronico_,direccion=direccion_;}
-    inline string getDar_datos_monitor(){return nombre_, apellidos_, DNI_, edad_, correoelectronico_, direccion_;}
 
     bool confirmar_monitor();
     string getDar_rutas();
     string getDar_fechayhoradisp();
-    void Dar_rutas_especiales();
-    void imprimirMonitores();
     void setReservar_fechayhora();
-
-
+    
+    void Dar_rutas_especiales();
+    string getDar_fechayhoraespecial();
+    void setReservar_fechayhoraespecial();
+    string imprimirMonitores();
 }
