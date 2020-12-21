b4fd46ab597cbb3cc35de7527481ee80f0c01644
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2020-12-21 15:42:11 +0100

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -38,8 +38,11 @@ public:
     bool confirmar_monitor();
     void dar_rutas();
     void dar_fechayhoradisp();
-    void reservar_fechayhora();
-    
+    void reservar_fechayhora1();
+    void reservar_fechayhora2();
+    void reservar_fechayhora3();
+    void reservar_fechayhora4();
+   
     void dar_rutas_especiales();
     void dar_fechayhoraespecial();
     void reservar_fechayhoraespecial();
