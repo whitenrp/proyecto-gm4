6db245552606a9bcfe1158db5fe5fa9ca5b265f3
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2020-12-21 16:07:59 +0100

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -6,34 +6,8 @@ using namespace std;
 
 class Monitor{
 private:
-int edad_;
-string nombre_;
-string apellidos_;
-string DNI_;
-string correoelectronico_;
-string direccion_;
 
 public:
-    Monitor(){
-        string DNI_="";
-        int edad_=0;
-        string nombre_="";
-        string apellidos_="";
-        string correoelectronico_="";
-        string direccion_="";
-    }
-    inline void setEdad(int edad){edad=edad_;}
-    inline int getEdad(){return edad_;}
-    inline void setNombre(string nombre){nombre=nombre_;}
-    inline string getNombre(){return nombre_;}
-    inline void setApellidos(string apellidos){apellidos=apellidos_;}
-    inline string getApellidos(){return apellidos_;}
-    inline void setDNI(string DNI){DNI=DNI_;}
-    inline string getDNI(){return DNI_;}
-    inline void setCorreoelectronico(string correoelectronico){correoelectronico=correoelectronico_;}
-    inline string getCorreoelectronico(){return correoelectronico_;}
-    inline void setDireccion(string direccion){direccion=direccion_;}
-    inline string getDireccion(){return direccion_;}
 
     bool confirmar_monitor();
     void dar_rutas();
